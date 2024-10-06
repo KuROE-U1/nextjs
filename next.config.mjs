c934bc6139997dec050cdaa8f31ca8e5cc4d174f
hiroto <hiroto.saoliker@gmail.com>
2024-10-07 00:16:59 +0900

    Update next.config.mjs for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    output: 'export', // 静的エクスポート
+    basePath: '/Nextjs', // リポジトリ名に合わせたベースパス
+    assetPrefix: '/Nextjs/', // 静的ファイルのパス
+    trailingSlash: true, // URLの末尾にスラッシュを追加
   };
   
   export default nextConfig;
+  
