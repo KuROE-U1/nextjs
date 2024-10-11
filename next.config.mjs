8c63f33007af5a0ac62205c01e924157ea3cbdbe
h.minakuchi <h.minakuchi@caremax.co.jp>
2024-10-11 10:43:33 +0900

    動画行けるか？

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export'
+    // 他の設定...
   };
   
   export default nextConfig;
-  
--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,11 @@
 "use client";
 
 import { useState, useEffect } from 'react';
-import Head from 'next/head';
-
-import './index.css';
 
 export default function Home() {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        // コンポーネントがマウントされた後、すぐに loading ステータスを false に設定
         setLoading(false);
     }, []);
 
@@ -22,38 +18,23 @@ export default function Home() {
     }
 
     return (
-        <>
-            <Head>
-                <title>index</title>
-                <meta name="description" content="index" />
-            </Head>
-            <article>
-                <section id="top">
-                    <div style={{ height: '100vh', backgroundColor: '#ccc', display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'relative', overflow: 'hidden' }}>
-                        <div className="text-animation logo">
-                            <span className="char">K</span>
-                            <span className="char">u</span>
-                            <span className="char">R</span>
-                            <span className="char">O</span>
-                            <span className="char">E</span>
-                            <span className="char">u</span>
-                            <span className="char">1</span>
-                        </div>
+        <article>
+            <section id="top">
+                <div style={{ height: '100vh', backgroundColor: '#ccc', display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'relative', overflow: 'hidden' }}>
+                    <video src="./videos/1.mp4" controls autoPlay muted playsInline />
+                    <div className="text-animation logo">
+                        <span className="char">K</span>
+                        <span className="char">u</span>
+                        <span className="char">R</span>
+                        <span className="char">O</span>
+                        <span className="char">E</span>
+                        <span className="char">u</span>
+                        <span className="char">1</span>
                     </div>
-                </section>
+                </div>
+            </section>
 
-                <section id="works">
-
-                </section>
-
-                <section id="about">
-
-                </section>
-                
-                <section id="contact">
-
-                </section>
-            </article>
-        </>
+            {/* 他のセクション... */}
+        </article>
     );
 }
